225ad37e25dcf30f82eff5d9026f61d800a479d9
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2022-12-13 21:52:58 +0900

    fix: Card style

--- components/card.tsx
+++ components/card.tsx
@@ -26,6 +26,7 @@ function imageElement(cardImage: CardImage | undefined): JSX.Element {
           maxWidth: "100%",
           height: "auto",
         }}
+        className="w-full"
       />
     );
   }
@@ -35,10 +36,10 @@ function imageElement(cardImage: CardImage | undefined): JSX.Element {
 export const Card: React.FC<Props> = ({ image, heading, content, url }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg relative m-2">
+      {imageElement(image)}
       <div className="px-6 py-4">
-        {imageElement(image)}
         <div className="font-bold text-xl mb-2">{heading}</div>
-        <p className="text-gray-700 text-base mb-2">{content}</p>
+        <p className="text-gray-700 text-base">{content}</p>
         <a href={url}>{">>"} GitHub</a>
       </div>
     </div>
