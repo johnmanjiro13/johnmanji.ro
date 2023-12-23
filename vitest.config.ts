b2fdae9040a2f4897119ab4b2511f44c875bc611
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2023-12-23 18:14:45 +0900

    fix: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
   plugins: [react()],
   test: {
     environment: "jsdom",
